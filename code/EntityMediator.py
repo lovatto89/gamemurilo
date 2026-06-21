70d1670d0d4d02b1544f6c0f59c7ed0d90259fa3
Fernando Lovatto <fernandolovatto.mariani@gmail.com>
2026-06-21 19:43:41 -0300

    Add Shot Player e Enemy

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,13 +1,23 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
 
+# DESTRUIR OS INIMIGOS QUANDO SAIR DA TELA
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
-            if ent.rect.right < 0:
+            if ent.rect.right <= 0:
+                ent.health = 0
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
                 ent.health = 0
         pass
 
---
+++ code/PlayerShot.py
@@ -0,0 +1,13 @@
+import pygame
+
+from code.Const import ENTITY_SPEED
+from code.Entity import Entity
+
+
+class PlayerShot(Entity):
+    def __init__(self, name: str, position):
+        super().__init__(name, position)
+
+
+    def move(self):
+        self.rect.centerx += ENTITY_SPEED[self.name]
